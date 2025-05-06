642b3dc8a11998d2580065cb02cd4cccc47ee73d
leonardoburalli <leonardoburalli1995@gmail.com>
2025-05-06 11:10:58 -0400

    webApp deployment

---
+++ server.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, render_template
+from EmotionDetection.emotion_detection import emotion_detector
+
+app = Flask("Emotion Detector")
+
+@app.route("/")
+def render_index_page():
+    return render_template('index.html')
+
+@app.route("/emotionDetector")
+def em_detector():
+
+    text_to_analyze = request.args.get('textToAnalyze')
+
+    response = emotion_detector(text_to_analyze)
+
+    emotion_values = [f"'{key}': {value}" for key, 
+    value in response.items() if key not in ("sadness" ,"dominant_emotion")]
+
+    result_string = f"For the given statement, the system response is {', '.join(emotion_values)}" \
+     f" and 'sadness': {response['sadness']}. The dominant emotion is {response['dominant_emotion']}."
+    return result_string
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=4444)
