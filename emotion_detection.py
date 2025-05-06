4845fb202cd39ecd3effdcda1b540844917aebf4
leonardoburalli <leonardoburalli1995@gmail.com>
2025-05-06 09:58:45 -0400

    Add new output from function

--- emotion_detection.py
+++ emotion_detection.py
@@ -6,4 +6,8 @@ def emotion_detector(text_to_analyze):
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     myobj = { "raw_document": { "text": text_to_analyze } }
     response = requests.post(url, json = myobj, headers=headers)
-    return response.text
+    dict_response = json.loads(response.text)
+    new_dict= dict_response['emotionPredictions'][0]['emotion']
+    max_key = max(new_dict, key=new_dict.get) 
+    new_dict['dominant_emotion'] = str(max_key) 
+    return new_dict
